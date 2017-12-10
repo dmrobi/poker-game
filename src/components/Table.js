69e7d253bcb1484ff62f312afb8ee7ccc47b7d1e
dmrobi <dmrobi89@gmail.com>
2017-12-10 16:20:36 +0600

    Table component create table in anywhere with json data and custom data object.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,86 +1,211 @@
-class Table extends Phaser.Plugin {
-	
-	draw(state, data) {
-		console.log("LobbyTable - Start");
-		for(let id in data){
-			state['tableRow'+id] = state.add.group();
-			let name = state.add.bitmapText(0, 0, 'custom-font', data[id].name, 54);
-			let buyIn = state.add.bitmapText(0, 75, 'custom-font', "Buy-In: " + data[id].buyInMin + "/" + data[id].buyInMin, 32);
-			let players = state.add.bitmapText(state.game.width - 450, 30, 'custom-font', data[id].playersJoined + "/" + data[id].playersLimit, 48);
-			let stakes = state.add.bitmapText(state.game.width - 300, 30, 'custom-font', data[id].stakesMin + "/" + data[id].stakesMin, 48);
+/**
+ * Phaser Table Plugin
+ * @author       Robiul Islam Robi <dmrobi89@gmail.com>
+ * @copyright    2017 Robiul Islam Robi
+ * @version 1.0.0
+ */
+export default class Table extends Phaser.Plugin {
+	constructor(game) {
+		super(game);
+		this.game = game;
 
-			/************************************************
-				Draw Pie Circle to show player joining status
-			************************************************/
-			let playerStatusRad = Math.ceil((data[id].playersJoined/data[id].playersLimit) * 360);
-			//Draw Inner circle
-			state['circle'+id] = state.add.graphics(state.game.width - 420, 50);
-			state['circle'+id].beginFill(0x00FF00, 1);
-			state['circle'+id].drawCircle(0, 0, 100);
-			state['circle'+id].endFill();
+		//Configure Table
+		this.config = {
+			width: this.game.width,
+			colWidth: [0, 0, 0],
+			rowHeight: 0,
+			dataKey: [],
+			paddingTop: 0,
+			paddingRight: 0,
+			paddingBottom: 0,
+			paddingLeft: 0
+		};
 
-			//Draw Status circle
-			state['pieCircle'+id] = state.add.graphics(state.game.width - 420, 50);
-		    state['pieCircle'+id].beginFill(0xFF0000, 1);
-		    if(playerStatusRad === 360){
-		    	state['pieCircle'+id].drawCircle(0, 0, 120);
-		    }else{
-		    	state['pieCircle'+id].arc(0, 0, 60, state.game.math.degToRad(playerStatusRad - 90), state.game.math.degToRad(360 - 90), true);
-		    }
-		    state['pieCircle'+id].endFill();
-		    
-		    //Adding all the column/data into table row
-			state['tableRow'+id].add(name);
-			state['tableRow'+id].add(buyIn);
-			state['tableRow'+id].add(state['pieCircle'+id]);
-			state['tableRow'+id].add(state['circle'+id]);
-			state['tableRow'+id].add(players);
-			state['tableRow'+id].add(stakes);
-			state['tableRow'+id].y = 160 * id;
+		//Setup table property
+		this.width = this.config.width;
+		this.height = 0;
+		
+		//Parent group to hold all the rows.
+		this.table = this.game.add.group();
+		this.rows = {}; //Rows array to hold all the generated rows.
+	}
 
-			//Adding newly created table row into table group
-			state.table.add(state['tableRow'+id]);
-			state['tableRow'+id] = undefined;
+	/**
+    * Create rows as per provided data
+    *
+    * @method Phaser.Plugin.Table#create
+    * @param {object}	[data= object]		- Json data object to be passed in this parameter.
+    * @param {number}	[rowHeight= 60]		- Set height for every rows.
+    */
+	create(data, rowHeight = this.config.rowHeight) {
+		for(let i in data){
+			let row = this.row(0, this.config.paddingTop + (rowHeight * i), data[i]);
+			this.height += rowHeight;
+
+			this.table.add(row);
 		}
-		console.log("LobbyTable - End");
+
+		//Return all the reows after creation
+		return this.rows = this.table.children;
+	}
+
+	/**
+    * Create rows as per provided data
+    *
+    * @method Phaser.Plugin.Table#row
+    * @param {number}	[positionX= 50]		- Set x position of the row.
+    * @param {number}	[positiony= 400]	- Set y position of the row.
+    * @param {object}	[data= object]		- Json data object to be passed in this parameter.
+    */
+	row(positionX, positoinY, data) {
+		let row = this.game.add.group();
+		
+		this.column(row, data); //Call column function to create columns with the json data object.
+		
+		row.x = positionX;
+		row.y = positoinY;
+
+		return row;
+	}
+
+	/**
+    * Create columns as per provided data
+    *
+    * @param {group}	[row= group]	- Empty phaser group to hold columns.
+    * @param {object}	[data= object]	- Json data object to be passed in this parameter.
+    */
+	column(row, data) {
+		//Initialize some variable and group.
+		let col = this.game.add.group();
+		let cells = {};
+		let tableWidth = this.game.width - (this.config.paddingLeft + this.config.paddingRight);
+		let totalWidthOfColumns = 0;
+		
+		//Loop through all the item of colWidth mentioned in the configure.
+		for(let id = 0; id < this.config.colWidth.length; id++){
+
+			//Creating text for every columns by taking data from json data object.
+			cells[id] = new Phaser.Text(this.game, 0, 0, data[this.config.dataKey[id]], {
+	            font: '48px Verdana',
+	            fill: 'black',
+	            align: 'left'
+	        });
+
+			cells[id].maxWidth = (tableWidth * this.config.colWidth[id]) / 100; //Defining column width in percentage.
+
+			//Set column position and anchor point for first and rest of the game objects in percentage.
+			if(id === 0) {
+				cells[id].anchor.setTo(0, 0.5);
+				cells[id].x = this.config.paddingLeft;
+			}else{
+				cells[id].anchor.setTo(0.5);
+				cells[id].x = Math.floor(totalWidthOfColumns + (cells[id].maxWidth / 2));
+			}
+
+			col.add(cells[id]);
+
+			//Update total width of collumn
+			totalWidthOfColumns += cells[id].maxWidth;
+		}
+
+		row.add(col);
+	}
+
+	/**
+    * Insert new row into table after crating base table or initialize.
+    *
+    * @method Phaser.Plugin.Table#addNewRow
+    * @param {array}	[columnWidth= []]	- Here an object should be passed with column width
+    * @param {array}	[gameObject= []]	- Here an object should be passed withy data of every column
+    * [columnWidth= []] and [gameObject= []] array should be in same length, because column is generating as per length of clumnWidth
+    * and content will inserted to the column with similar order.
+    */
+	addNewRow(columnWidth, gameObject){
+		//Check column is passed or not.
+		if(columnWidth){
+			let totalWidthOfColumns = 0; //Increasing this value on every column generation with column width
+			let row = this.game.add.group(); //Group hold all the generated columns.
+
+			//Check total length of colum and take individual action for single and multiple column
+			if(columnWidth.length == 1){
+				row.add(gameObject); //Adding single column into the row group.
+				row.y = this.height + this.config.paddingTop; //Set the y position of the row after all existing rows.
+
+				this.table.add(row); //Adding the newwly created row into table group.
+			}else{
+				let cols = []; //Temp variable to store all the generated columns.
+				
+				//Loop through all the clumns and insert them into a row to update table.
+				for(let i = 0; i < columnWidth.length; i++){
+					let cellWidth = (this.config.width * columnWidth[i]) / 100; //Setting individual cell width in percentage.
+					cols[i] = this.game.add.group(); //Creating group for every column.
+					cols[i].add(gameObject[i]); //Adding every game object in the colum.
+
+					//Set column position and anchor point for first and rest of the game objects in percentage.
+					if(i === 0) {
+						gameObject[i].anchor.setTo(0, 0.5);
+						cols[i].x = this.config.paddingLeft;
+					}else{
+						gameObject[i].anchor.setTo(0.5);
+						cols[i].x = Math.floor(totalWidthOfColumns + (cellWidth / 2));
+					}
+
+					row.add(cols[i]); //Adding all the columns into the row.
+
+					totalWidthOfColumns += cellWidth; //Update totalWidthOfColumns.
+				}
+
+				row.y = this.height + this.config.paddingTop; //Set the row y position after all the existing rows.
+				this.table.add(row); //Adding the row into the table
+				this.height += this.config.rowHeight; //Update the original table height with new row height.
+
+				cols = undefined; //Reset the temp cols variable to its original.
+			}
+
+		}else{
+			console.log('This function received only object as parameter.');
+		}
+		
+	}
+
+
+	/**
+    * Change Default Settings of the table
+    *
+    * @method Phaser.Plugin.Table#configure
+    * @param {Object}	[options] - Object that contain properties to change the behavior of the plugin.
+    * @param {number}	[options.width=this.game.width]	- The width of the table.
+    * @param {array}	[options.colWidth=[60, 30, 10]]	- Width of the individual Column in percentage and Sum of the all valus should be 100
+    * @param {number}	[options.rowHeight=60]			- Height of individual row.
+    * @param {array}	[options.dataKey=['name', 'buyInMin', 'playersJoined']]	- All the keys of json data to show in the table.
+    * @param {number}	[options.paddingTop=400]		- Leave empty space Avobe of the table before start drawing.
+    * @param {number}	[options.paddingRight=50]		- Leave empty space Right of the table before start drawing.
+    * @param {number}	[options.paddingBottom=0]		- Leave empty space Bottom of the table before start drawing.
+    * @param {number}	[options.paddingLeft=50]		- Leave empty space Left of the table before start drawing.
+    */
+    configure (options) {
+
+        if (options) {
+            for (let property in options) {
+                if (this.config.hasOwnProperty(property)) {
+                    this.config[property] = options[property];
+                }
+            }
+        }
+
+    };
+
+    /**
+    * Get the length of an object
+    *
+    * @method this.ObjLength()
+    * @param {Object}	[object] - Object to get its length.
+    */
+    objLength(object) {
+	    let size = 0, key;
+	    for (key in object) {
+	        if (object.hasOwnProperty(key)) size++;
+	    }
+	    return size;
 	}
 }
-
-export default Table;
-
-/*
-for(let id in this.tablesData){
-	let playerStatus = this.tablesData[id].playersJoined * 100 / this.tablesData[id].playersLimit;
-	let playerStatusRad = this.game.math.floorTo(playerStatus / 100 * 360);
-
-	this['tableRow'+id] = this.add.group();
-	let name = this.add.bitmapText(0, 0, 'custom-font', this.tablesData[id].name, 54);
-	let buyIn = this.add.bitmapText(0, 75, 'custom-font', "Buy-In: " + this.tablesData[id].buyInMin + "/" + this.tablesData[id].buyInMin, 32);
-	let players = this.add.bitmapText(this.game.width - 450, 30, 'custom-font', this.tablesData[id].playersJoined + "/" + this.tablesData[id].playersLimit, 48);
-	let stakes = this.add.bitmapText(this.game.width - 300, 30, 'custom-font', this.tablesData[id].stakesMin + "/" + this.tablesData[id].stakesMin, 48);
-
-	//Draw Pie Circle for players status
-	this['circle'+id] = this.add.graphics(this.game.width - 420, 50);
-	this['circle'+id].beginFill(0x00FF00, 1);
-	this['circle'+id].drawCircle(0, 0, 100);
-	this['circle'+id].endFill();
-
-	this['pieCircle'+id] = this.add.graphics(this.game.width - 420, 50);
-    this['pieCircle'+id].beginFill(0xFF0000, 1);
-    this['pieCircle'+id].arc(0, 0, 60, this.game.math.degToRad(playerStatusRad), this.game.math.degToRad(360-playerStatusRad), true);
-    //this['pieCircle'+id].endFill();
-    
-    //Adding all the column/data into table row
-	this['tableRow'+id].add(name);
-	this['tableRow'+id].add(buyIn);
-	this['tableRow'+id].add(this['pieCircle'+id]);
-	this['tableRow'+id].add(this['circle'+id]);
-	this['tableRow'+id].add(players);
-	this['tableRow'+id].add(stakes);
-	this['tableRow'+id].y = 160 * id;
-
-	//Adding newly created table row into table group
-	this.table.add(this['tableRow'+id]);
-	this['tableRow'+id] = undefined;
-}
-*/
--- src/states/Lobby.js
+++ src/states/Lobby.js
@@ -1,9 +1,12 @@
-import Table from 'components/Table';
+import Table from 'components/Table'; //Import table component
 
 class Lobby extends Phaser.State {
 	
 	create(){
 		console.log("State - Lobby");
+
+		//Load the json data into the game
+		this.tablesData = this.cache.getJSON('tablesData');
 
 		//Start and Configure kinetic scroll plugin
 		this.game.kineticScrolling.start();
@@ -17,30 +20,45 @@ class Lobby extends Phaser.State {
 			deltaWheel: 40
 		});
 
-		//Load the json data into the game
-		this.tablesData = this.cache.getJSON('tablesData');
+		//Create and Configure Table Comonent
+		this.lobbyTable = new Table(this.game);
+		this.lobbyTable.configure({
+			width: this.game.width,
+			colWidth: [60, 30, 10],
+			rowHeight: 60,
+			dataKey: ['name', 'buyInMin', 'playersJoined'],
+			paddingTop: 400,
+			paddingRight: 50,
+			paddingBottom: 0,
+			paddingLeft: 50
+		});
 
-		//Create table group to hold all the table rows
-		this.table = this.add.group();
+		//Create Table with Json Data.
+		this.lobbyTable.create(this.tablesData);
 
-		//Draw table with Table component
-		this.lobbyTable = new Table;
-		this.lobbyTable.draw(this, this.tablesData);
+		/* Add new seperate row in the same table with different data and columns structure. */
+		//Generating New Data
+		this.text1 = this.add.bitmapText(0, 0, 'custom-font', "Text 1", 54);
+		this.text2 = this.add.bitmapText(0, 0, 'custom-font', "Text 2", 54);
+		this.text3 = this.add.bitmapText(0, 0, 'custom-font', "Text 3", 54);
+		this.text4 = this.add.bitmapText(0, 0, 'custom-font', "Text 4", 54);
+		
+		//Create and Insert new Row into the existing table this.lobbyTable;
+		this.lobbyTable.addNewRow([25, 25, 25, 25], [this.text1, this.text2, this.text3, this.text4]);
 
-		//Header and Footer
+		//Create individual row with different position and data as like parent table data structure.
+		let row = this.lobbyTable.row(0, 1100, {name:"Robi", buyInMin:30, playersJoined:6});
+
+		//Simple Header and Footer Image
 		this.header = this.add.sprite(0, 0, "header");
 		this.footer = this.add.sprite(0, this.game.height-315, "footer");
-
-		//Adjust table position
-		this.table.x = 50;
-		this.table.y = this.header.height + 50;
-
-		//Change game size implement scroll effect
-		this.world.resize(window.innerWidth, this.table.height + this.header.height + this.footer.height + 100);
 
 		//Make some object fixed to camera or non movable
 		this.header.fixedToCamera = true;
 		this.footer.fixedToCamera = true;
+
+		//Change game size implement scroll effect
+		this.world.resize(window.innerWidth, this.lobbyTable.height + this.header.height + this.footer.height + 100);
 	}
 
 }
